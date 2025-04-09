d1bd97a3a7d49d3193fe91f30516b237846816f7
Dennis Van Hout <vanhoutdennis@gmail.com>
2025-04-09 10:54:10 +0200

    Added missing tests

--- MicrowaveOvenController.Tests/MicrowaveControllerTests.cs
+++ MicrowaveOvenController.Tests/MicrowaveControllerTests.cs
@@ -14,11 +14,46 @@ namespace MicrowaveOvenController.Tests
         }
 
         [Fact]
+        public void WhenDoorIsOpen_ShouldTurnOnLight()
+        {
+            _controller.OnDoorOpenChanged(true);
+            Assert.True(_controller.LightOn);
+        }
+
+        [Fact]
+        public void WhenDoorIsClosed_ShouldTurnOffLight()
+        {
+            _controller.OnDoorOpenChanged(false);
+            Assert.False(_controller.LightOn);
+        }
+
+        [Fact]
+        public void OpenDoor_WhenHeating_ShouldStopHeater()
+        {
+            _mockService.Setup(s => s.IsHeating()).Returns(true);
+            _controller.OnDoorOpenChanged(true);
+            _mockService.Verify(s => s.StopHeating(), Times.Once);
+        }
+
+        [Fact]
         public void StartButtonPressed_WhenDoorIsOpen_ShouldDoNothing()
         {
             _mockService.Setup(s => s.IsDoorOpen()).Returns(true);
             _controller.OnStartButtonPressed(null, EventArgs.Empty);
             _mockService.Verify(s => s.StartHeating(), Times.Never);
+        }
+
+        [Fact]
+        public void StartButtonPressed_WhenDoorIsClosed_ShouldStartHeatingForOneMinute()
+        {
+            _mockService.Setup(s => s.IsDoorOpen()).Returns(false);
+            _mockService.Setup(s => s.IsHeating()).Returns(false);
+            _mockService.Setup(s => s.RemainingTime()).Returns(60);
+
+            _controller.OnStartButtonPressed(null, EventArgs.Empty);
+
+            _mockService.Verify(s => s.StartHeating(), Times.Once);
+            Assert.Equal(60, _controller.RemainingTime);
         }
 
         [Fact]
